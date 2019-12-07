441ed2bc8bcc0ccde8412cc2d01a15ee7a8da412
danacr <dan@acristinii.com>
2019-12-07 21:14:25 +0100

    first commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM golang:1.13-alpine as builder
+
+WORKDIR /workspace
+
+# Copy the go source
+COPY main.go main.go
+
+# Build
+RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 GO111MODULE=on go build -a -o server main.go
+
+# Use distroless as minimal base image to package the manager binary
+# Refer to https://github.com/GoogleContainerTools/distroless for more details
+FROM gcr.io/distroless/static:nonroot
+WORKDIR /
+COPY --from=builder /workspace/server .
+COPY favicon.ico .
+ENTRYPOINT ["/server"]
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// Device struct
+type Device struct {
+	Name string
+	ID   string
+}
+
+// Phone looks better capitalized
+var Phone Device
+
+func faviconHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "favicon.ico")
+}
+
+func disrupt(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		js, err := json.Marshal(Device{
+			Name: Phone.Name,
+			ID:   Phone.ID,
+		})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		_, err = w.Write(js)
+		if err != nil {
+			fmt.Printf("Error %q", err)
+		}
+		return
+	case "POST":
+		err := json.NewDecoder(r.Body).Decode(&Phone)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		http.StatusText(200)
+	default:
+		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
+	}
+}
+
+func main() {
+	http.HandleFunc("/", disrupt)
+	http.HandleFunc("/favicon.ico", faviconHandler)
+	fmt.Printf("Starting Disrupter\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
