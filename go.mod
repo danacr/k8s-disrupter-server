f7fe48be3d557e1a915e720be7f634f158d877ba
danacr <dan@acristinii.com>
2020-03-01 23:45:41 +0100

    Surgical version 💉

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module githubn.com/danacr/k8s-disrupter-server
 
 go 1.13
 
-require golang.org/x/tools v0.0.0-20200228224639-71482053b885 // indirect
+require (
+	cloud.google.com/go v0.38.0
+	golang.org/x/net v0.0.0-20190620200207-3b0461eec859
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+	golang.org/x/tools v0.0.0-20200228224639-71482053b885 // indirect
+	google.golang.org/api v0.19.0
+)
--- main.go
+++ main.go
@@ -8,6 +8,12 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
+
+	"cloud.google.com/go/pubsub"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/compute/v1"
 )
 
 // Device struct
@@ -47,11 +53,27 @@ func disrupt(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
-		err = rebootnode(os.Getenv("GREMLIN_TEAM_ID"), os.Getenv("GREMLIN_API_KEY"))
+		// err = rebootnode(os.Getenv("GREMLIN_TEAM_ID"), os.Getenv("GREMLIN_API_KEY"))
+		instancelist, err := getinstances()
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
+		var goodbye string
+		for _, instanceWithNamedPorts := range instancelist {
+			if instanceWithNamedPorts.Status == "RUNNING" {
+				goodbye = instanceWithNamedPorts.Instance
+			}
+		}
+		goodbye = goodbye[strings.LastIndex(goodbye, "/")+1:]
+
+		result, err := deleteinstance(goodbye)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		log.Println(result)
+
 		http.StatusText(200)
 
 	default:
@@ -67,6 +89,9 @@ func main() {
 	}
 	if os.Getenv("GREMLIN_API_KEY") == "" {
 		err = errors.New("GREMLIN_API_KEY env var required")
+		log.Fatal(err)
+	}
+	if err = serviceAccount(); err != nil {
 		log.Fatal(err)
 	}
 	http.HandleFunc("/", disrupt)
@@ -110,4 +135,82 @@ func rebootnode(teamid, apikey string) error {
 	defer resp.Body.Close()
 	log.Panicln("Triggered Node Reboot")
 	return nil
+}
+
+// serviceAccount shows how to use a service account to authenticate.
+func serviceAccount() error {
+	// Download service account key per https://cloud.google.com/docs/authentication/production.
+	// Set environment variable GOOGLE_APPLICATION_CREDENTIALS=/path/to/service-account-key.json
+	// This environment variable will be automatically picked up by the client.
+	client, err := pubsub.NewClient(context.Background(), "your-project-id")
+	if err != nil {
+		return fmt.Errorf("pubsub.NewClient: %v", err)
+	}
+	// Use the authenticated client.
+	_ = client
+
+	return nil
+}
+
+func getinstances() ([]*compute.InstanceWithNamedPorts, error) {
+	ctx := context.Background()
+
+	c, err := google.DefaultClient(ctx, compute.CloudPlatformScope)
+	if err != nil {
+		return nil, err
+	}
+
+	computeService, err := compute.New(c)
+	if err != nil {
+		return nil, err
+	}
+
+	// Project ID for this request.
+	project := "kill-the-cluster"
+
+	// The name of the zone where the instance group is located.
+	zone := "europe-west6-a" //
+
+	// The name of the instance group from which you want to generate a list of included instances.
+	instanceGroup := "gke-chaotic-cluster-preemptible-pool-ae9c7dfb-grp"
+
+	rb := &compute.InstanceGroupsListInstancesRequest{}
+	var instancelist []*compute.InstanceWithNamedPorts
+	req := computeService.InstanceGroups.ListInstances(project, zone, instanceGroup, rb)
+	if err := req.Pages(ctx, func(page *compute.InstanceGroupsListInstances) error {
+		instancelist = append(instancelist, page.Items...)
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+	return instancelist, nil
+}
+
+func deleteinstance(instance string) (string, error) {
+	ctx := context.Background()
+
+	c, err := google.DefaultClient(ctx, compute.CloudPlatformScope)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	computeService, err := compute.New(c)
+	if err != nil {
+		return "", err
+	}
+
+	// Project ID for this request.
+	project := "kill-the-cluster"
+
+	// The name of the zone where the instance group is located.
+	zone := "europe-west6-a" //
+
+	resp, err := computeService.Instances.Delete(project, zone, instance).Context(ctx).Do()
+	if err != nil {
+		return "", err
+	}
+
+	// TODO: Change code below to process the `resp` object:
+	result := fmt.Sprintf("%#v\n", resp)
+	return result, nil
 }
